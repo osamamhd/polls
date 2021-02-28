80fef4132f01e7752f6101df30fb049b077efed7
Osama Mhd <osamamhd82@gmail.com>
2021-02-28 18:45:31 +0200

    Contact Ajax View.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import ContactForm
+from django.views import View
 
-# Create your views here.
+
+class ContactAjax(View):
+	form_class = ContactForm
+	template_name = "index.html"
+
+	def get(self, *args, **kwargs):
+		form = self.form_class()
+		return render(self.request, self.template_name, {"contactForm": form})
+
+	def post(self, *args, **kwargs):
+		if self.request.method == "POST" and self.request.is_ajax():
+			form = self.form_class(self.request.POST)
+			form.save()
+			return JsonResponse({"success":True}, status=200)
+		return JsonResponse({"success":False}, status=400)
